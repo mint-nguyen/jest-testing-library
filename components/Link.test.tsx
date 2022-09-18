51862b88defa181997261d6f3ea822c622905e26
Mint <mintnguyen.pa@gmail.com>
2022-09-18 12:25:44 -0700

    feat: setup jest

---
+++ components/Link.test.tsx
@@ -0,0 +1,18 @@
+import renderer from "react-test-renderer";
+import Link from "./Link";
+
+it("changes the class when hovered", () => {
+	const component = renderer.create(
+		<Link page="http://www.facebook.com">Facebook</Link>
+	);
+	let tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+
+	// re-rendering
+	tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+
+	// re-rendering
+	tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
