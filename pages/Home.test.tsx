adf622932c15532f22f59049ccd8b7f454664c0d
Mint <mintnguyen.pa@gmail.com>
2022-09-19 14:40:35 -0700

    feat: set up enzyme adapter

---
+++ pages/Home.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Home from "./index";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without crashing", () => {
+	const wrapper = shallow(<Home />);
+	const homeComponent = wrapper.find("[data-test='component-home']");
+	expect(homeComponent.length).toBe(1);
+});
+
+test("renders increment button", () => {});
+
+test("renders counter display", () => {});
+
+test("counter starts at 0", () => {});
+
+test("clicking button increments counter display", () => {});
